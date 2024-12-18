47db8316b47a38d5024c746d208cd0bbec8f7aa9
Ola <gain@Ganius-MacBook-Pro.local>
2024-12-17 21:33:21 -0600

    player class init

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    isInit = pygame.get_init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
+    
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    dt = 0
+    
+ 
 
     while running:
-       
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return
 
+        for obj in updatable:
+            obj.update(dt)
         # Fill the screen with black
         screen.fill((0, 0, 0)) 
-        # pygame.time.wait(2000)
-        
-        print(f"Is pygame initialized: {isInit}")
-        print("Starting Asteroid Game")
-        print (f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
+       
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60)/1000
+
+        
+        
+       
+
 
 if __name__ == "__main__":
     main()
